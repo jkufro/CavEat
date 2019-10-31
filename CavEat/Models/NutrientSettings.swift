5ea8f69c25f519fe43673dbab7285d786ab72f43
Darien <dweems@andrew.cmu.edu>
2019-10-31 01:03:15 -0400

    Added default nutrient settings for common nutrients

--- CavEat/Models/NutrientSettings.swift
+++ CavEat/Models/NutrientSettings.swift
@@ -15,9 +15,18 @@ class NutrientSettings {
 
     private init() {
         // set the defaults
+        // nutrients with a dailyValue of 0 do not have a default DV provided by FDA
         nutrientDictionary = [
-            "Added Sugars": NutrientSetting(name: "Added Sugars", unit: "g", dailyValue: 32),
-            "Dietary Fiber": NutrientSetting(name: "Dietary Fiber", unit: "g", dailyValue: 25)
+            "Added Sugars": NutrientSetting(name: "Added Sugars", unit: "g", dailyValue: 0),
+            "Cholesterol": NutrientSetting(name: "Cholesterol", unit: "mg", dailyValue: 300),
+            "Dietary Fiber": NutrientSetting(name: "Dietary Fiber", unit: "g", dailyValue: 25),
+            "Protein": NutrientSetting(name: "Protein", unit: "g", dailyValue: 50),
+            "Saturated Fat": NutrientSetting(name: "Saturated Fat", unit: "g", dailyValue: 20),
+            "Sodium": NutrientSetting(name: "Sodium", unit: "mg", dailyValue: 2300),
+            "Total Carbohydrates": NutrientSetting(name: "Total Carbohydrates", unit: "g", dailyValue: 300),
+            "Total Fat": NutrientSetting(name: "Total Fat", unit: "g", dailyValue: 65),
+            "Total Sugars": NutrientSetting(name: "Total Sugars", unit: "g", dailyValue: 0),
+            "Trans Fat": NutrientSetting(name: "Trans Fat", unit: "g", dailyValue: 0)
         ]
     }
 
