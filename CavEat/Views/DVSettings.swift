c80922e64a716571167e3ac4420b7367342c0a04
Justin Kufro <kufroj@gmail.com>
2019-11-04 22:11:33 -0500

    text in settings modal

--- CavEat/Views/DVSettings.swift
+++ CavEat/Views/DVSettings.swift
@@ -42,7 +42,17 @@ struct DVSettings: View {
                             }
                             Spacer()
                         }
-                        Text("Hello")
+                        Text("Daily Value Settings")
+                            .font(.title)
+                            .padding(.bottom)
+                        VStack(alignment: .leading) {
+                            Text("You are able to personalize the desired daily value intake of all the nutrients listed on this page. Any changes you make here will be reflected in scan results through different percentage daily values (%DV).")
+                                .padding(.bottom)
+                            Text("You are always able to reset your changes back to the app default value by pressing the “Reset to Default” button.")
+                                .padding(.bottom)
+                            Text("All values default to the average adult consumption guidelines.")
+                                .padding(.bottom)
+                        }
                         Spacer()
                     }.padding()
                 }
